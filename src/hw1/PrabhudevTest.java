5d70b38e138ce4e0f825789669e95ad51ef79322
Shreyas Prabhudev <31231882+shreyasprabhudev@users.noreply.github.com>
2023-02-14 12:48:00 -0800

    Create PrabhudevTest.java

---
+++ src/hw1/PrabhudevTest.java
@@ -0,0 +1,6 @@
+package hw1;
+
+public static void main(String[] args){
+        Student Shreyas = new Student("Shreyas", "I am a second year CS major attending SJSU.", 015466750, COMPUTER_SCIENCE, 03/13/03);
+        System.out.println(Shreyas);
+}
